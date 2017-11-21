57152e6ee7ec95ab46e39177520fb1e85b6f9302
maikonf <32256261+maikonf@users.noreply.github.com>
2017-11-21 21:18:33 -0200

    Update app.js

--- cliente_api_serasa/app.js
+++ cliente_api_serasa/app.js
@@ -3,6 +3,5 @@ var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
 app.listen(5000, function(){
-	var ip_server_api = 'http://localhost:3000/';
 	console.log('Cliente Serasa online');
 })
